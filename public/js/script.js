e70244ab1338b02d7af795bf90a447a37e5bc82d
Miguel Cruz <79615087+MiguelXCruz@users.noreply.github.com>
2021-05-14 11:05:48 -0300

    OK

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,21 @@
-function imgSlider(anything){
-    document.querySelector('.starbucks').src = anything;
+function imgSlider(imageProduct){
+    document.querySelector('.starbucks').src = imageProduct;
+
+}   
+
+function changeCirclecolor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+
+}
+
+
+function toggleMenu (){
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+
+    console.log("toggleButtonActived")
 }
